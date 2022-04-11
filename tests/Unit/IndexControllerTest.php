f8f34f6295cae4149fd6bde2020d8af8e0e9cff6
SergM2014 <weisse011@gmail.com>
2022-04-11 18:00:15 +0300

    index@index test in progress

--- tests/Unit/IndexControllerTest.php
+++ tests/Unit/IndexControllerTest.php
@@ -3,27 +3,44 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use App\Repositories\SliderRepo;
+use App\Repositories\CarouselRepo;
+use App\Repositories\CategoryRepo;
 use App\Http\Controllers\IndexController;
 
 class IndexControllerTest extends TestCase
 {
-//    /**
-//     * A basic unit test example.
-//     *
-//     * @return void
-//     */
-//    public function test_example()
-//    {
-//        $this->assertTrue(true);
-//    }
+    protected function setUp(): void
+    {
+        $this->slider = $this->getMockBuilder(SliderRepo::class)
+            ->onlyMethods(['getAll'])
+            ->getMock();
+        $this->slider->expects($this->once())
+            ->method('getAll');
+           // ->willReturn(true);
+
+        $this->carousel = $this->getMockBuilder(CarouselRepo::class)
+            ->onlyMethods(['getAll'])
+            ->getMock();
+        $this->carousel->expects($this->once())
+            ->method('getAll');
+            //->willReturn(true);
+
+        $this->category = $this->getMockBuilder(CategoryRepo::class)
+            ->onlyMethods(['getVerticalMenu'])
+            ->getMock();
+        $this->category->expects($this->once())
+            ->method('getVerticalMenu');
+           // ->willReturn(true);
+    }
+
     public function test_index(): void
     {
       $index = new IndexController();
 
-      $index->index();
+      $response = $index->index($this->slider, $this->carousel, $this->category);
 
-      $this->assertTrue(true);
+      $response ->assertOk();
     }
-
 
 }
