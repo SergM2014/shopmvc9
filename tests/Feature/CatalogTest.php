a0ebedbd29f502b5079ac33ca29e79e4808be472
SergM2014 <weisse011@gmail.com>
2022-04-11 11:59:30 +0300

    feature tests for catalogController are added

---
+++ tests/Feature/CatalogTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CatalogTest extends TestCase
+{
+    public function test_index()
+    {
+        $response = $this->get('/catalog/all');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_showCategories()
+    {
+        $response = $this->get('/catalog/category/category1');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_showManufactures()
+    {
+        $response = $this->get('/catalog/manufacturer/siemens');
+
+        $response->assertStatus(200);
+    }
+}
