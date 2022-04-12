82fb931690146d97969f5f724186dce8784774b2
SergM2014 <weisse011@gmail.com>
2022-04-12 17:45:11 +0300

    unit IndexControllerTest@ testAboutUs +

--- tests/Unit/IndexControllerTest.php
+++ tests/Unit/IndexControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use Illuminate\View\View;
+use App\Repositories\BackgroundRepo;
 use Illuminate\Contracts\View\Factory;
 use PHPUnit\Framework\TestCase;
 use App\Repositories\SliderRepo;
@@ -15,25 +16,17 @@ class IndexControllerTest extends TestCase
 {
     protected function setUp(): void
     {
-        $this->slider = $this->getMockBuilder(SliderRepo::class)
-            ->onlyMethods(['getAll'])
-            ->getMock();
-        $this->slider->expects($this->once())
-            ->method('getAll');
 
-        $this->carousel = $this->getMockBuilder(CarouselRepo::class)
-            ->onlyMethods(['getAll'])
-            ->getMock();
-        $this->carousel->expects($this->once())
-            ->method('getAll');
+//        $this->background = $this->getMockBuilder(BackgroundRepo::class)
+//            ->onlyMethods(['aboutUs'])
+//            ->getMock();
+//        $this->background->expects($this->once())
+//            ->method('aboutUs');
 
-        $this->category = $this->getMockBuilder(CategoryRepo::class)
-            ->onlyMethods(['getVerticalMenu'])
-            ->getMock();
-        $this->category->expects($this->once())
-            ->method('getVerticalMenu');
+    }
 
-
+    public function testIndex(): void
+    {
         $view = $this->createMock(View::class);
         $mockFactory = $this->createMock(Factory::class);
         app()->instance(Factory::class, $mockFactory);
@@ -41,10 +34,44 @@ class IndexControllerTest extends TestCase
             ->method('make')
             ->with('custom.index')
             ->willReturn($view);
+
+        $slider = $this->getMockBuilder(SliderRepo::class)
+            ->onlyMethods(['getAll'])
+            ->getMock();
+        $slider->expects($this->once())
+            ->method('getAll');
+
+        $carousel = $this->getMockBuilder(CarouselRepo::class)
+            ->onlyMethods(['getAll'])
+            ->getMock();
+        $carousel->expects($this->once())
+            ->method('getAll');
+
+        $category = $this->getMockBuilder(CategoryRepo::class)
+            ->onlyMethods(['getVerticalMenu'])
+            ->getMock();
+        $category->expects($this->once())
+            ->method('getVerticalMenu');
+
+        (new IndexController())->index($slider, $carousel, $category);
     }
 
-    public function testIndex(): void
+    public function testAboutUs(): void
     {
-        (new IndexController())->index($this->slider, $this->carousel, $this->category);
+        $view = $this->createMock(View::class);
+        $mockFactory = $this->createMock(Factory::class);
+        app()->instance(Factory::class, $mockFactory);
+        $mockFactory->expects($this->any())
+            ->method('make')
+            ->with('custom.aboutUs')
+            ->willReturn($view);
+
+        $background = $this->getMockBuilder(BackgroundRepo::class)
+            ->onlyMethods(['aboutUs'])
+            ->getMock();
+        $background->expects($this->once())
+            ->method('aboutUs');
+
+        (new IndexController())->aboutUs($background);
     }
 }
