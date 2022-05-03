c2273f9ce8f0fb425205e719734c59903cbb09ad
SergM2014 <weisse011@gmail.com>
2022-05-03 10:27:55 +0300

    busketController@show tests +

--- tests/Unit/BusketControllerTest.php
+++ tests/Unit/BusketControllerTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Unit;
 
+use App\Product;
 use Illuminate\View\View;
 use PHPUnit\Framework\TestCase;
+use App\Repositories\ProductRepo;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Session\SessionManager;
 use App\Http\Controllers\BusketController;
 use Illuminate\Database\Eloquent\Collection;
 
@@ -29,7 +32,7 @@ class BusketControllerTest extends TestCase
         (new BusketController())->showOrderForm();
     }
 
-    public function testSucceededOrder():void
+    public function testSucceededOrder(): void
     {
         $mockFactory = $this->createMock(Factory::class);
         app()->instance(Factory::class, $mockFactory);
@@ -39,5 +42,43 @@ class BusketControllerTest extends TestCase
             ->willReturn($this->createMock(View::class));
 
         (new BusketController())->succeededOrder();
+    }
+
+    public function testShow(): void
+    {
+        $viewClass = $this->createMock(View::class);
+        $viewClass->expects($this->once())
+            ->method('with')
+            ->willReturn($this->createMock(View::class));
+
+        $mockFactory = $this->createMock(Factory::class);
+        $mockFactory->expects($this->once())
+            ->method('make')
+            ->with('custom.modalWindow.bigBusketContent')
+             ->willReturn($viewClass);
+
+        app()->instance(Factory::class, $mockFactory);
+
+        $DbCollection = $this->createMock(Collection::class);
+
+        $productRepo = $this->getMockBuilder(ProductRepo::class)
+            ->onlyMethods(['findItems'])
+            ->getMock();
+        $productRepo->expects($this->once())
+            ->method('findItems')
+            ->willReturn($DbCollection);
+
+        $session = $this->getMockBuilder(SessionManager::class)
+            ->addMethods(['get'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $session->expects($this->once())
+            ->method('get')
+            ->willReturn([]);
+
+
+        app()->instance('session', $session);
+
+        (new BusketController())->show($productRepo);
     }
 }
